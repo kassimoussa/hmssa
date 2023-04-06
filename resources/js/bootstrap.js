84bce83265d31c626a191b35ef393e55a0287f88
kassimoussa <kassim.moussali@gmail.com>
2023-04-06 12:02:32 +0300

    first commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,11 @@
+import 'bootstrap';
 import _ from 'lodash';
 window._ = _;
 
 import * as Popper from '@popperjs/core'
 window.Popper = Popper
 
-import * as bootstrap from '@bootstrap';
+import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
 
 
